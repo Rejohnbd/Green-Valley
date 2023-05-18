0e5818901c927a00f86567e39074e288b1144e78
rejohnbd <rejohn.cse@gmail.com>
2023-05-19 01:20:27 +0600

    Add All Api controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthenticateController;
+use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\StaffController;
+use App\Http\Controllers\Api\VentureController;
+use App\Http\Controllers\Api\VenturePlotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +25,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthenticateController::class, 'user']);
     Route::post('/logout', [AuthenticateController::class, 'logout']);
     Route::apiResource('staffs', StaffController::class);
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('ventures', VentureController::class);
+    Route::apiResource('venture-plots', VenturePlotController::class);
 });
